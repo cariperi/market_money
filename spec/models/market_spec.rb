b0b83f5baa33b81a56767a052aaec95a33bca3a5
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-15 19:36:29 -0400

    test: add test for vendor count instance method on model class

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe Market, type: :model do
     it { should have_many(:market_vendors) }
     it { should have_many(:vendors).through(:market_vendors) }
   end
+
+  describe 'instance methods' do
+    describe '#vendor_count' do
+      it 'should return the number of vendors associated with a given market' do
+        market_1 = create(:market)
+        market_2 = create(:market)
+        market_3 = create(:market)
+
+        vendor_1 = create(:vendor)
+        vendor_2 = create(:vendor)
+        vendor_3 = create(:vendor)
+
+        MarketVendor.create!(market: market_1, vendor: vendor_1)
+        MarketVendor.create!(market: market_1, vendor: vendor_2)
+        MarketVendor.create!(market: market_2, vendor: vendor_3)
+
+        expect(market_1.vendor_count).to eq(2)
+        expect(market_2.vendor_count).to eq(1)
+        expect(market_3.vendor_count).to eq(0)
+      end
+    end
+  end
 end
