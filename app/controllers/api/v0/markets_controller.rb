785ff571ef4872c89c9abec4336d6fc79fb4c245
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-15 19:59:57 -0400

    feat: update controller index action to remove local var

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,6 +1,5 @@
 class Api::V0::MarketsController < ApplicationController
   def index
-    markets = Market.all
-    render json: MarketSerializer.format_markets(markets)
+    render json: MarketSerializer.format_markets(Market.all)
   end
 end
