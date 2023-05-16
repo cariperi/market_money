7e0fd31534077d8fd5e56cb8b3f43f38a78929c8
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-16 15:48:13 -0400

    feat: set up market vendors controller add index method with happy and sad outcomes

---
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -0,0 +1,10 @@
+class Api::V0::MarketVendorsController < ApplicationController
+  def index
+    market = MarketsFacade.find_market(params[:market_id])
+    if market.class == Market
+      render json: VendorSerializer.new(market.vendors)
+    else
+      render json: ErrorSerializer.format_error(market), status: 404
+    end
+  end
+end
