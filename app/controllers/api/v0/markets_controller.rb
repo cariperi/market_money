f4c04a4c11909e5add3e404bf18942a8bc83073c
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-15 22:26:05 -0400

    feat: update show action in markets controller

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -2,4 +2,13 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render json: MarketSerializer.format_markets(Market.all)
   end
+
+  def show
+    market = MarketsFacade.find_market(params[:id])
+    if market.class == Market
+      render json: MarketSerializer.format_market(market)
+    else
+      render json: ErrorMarketSerializer.format_error_market(market), status: 404
+    end
+  end
 end
