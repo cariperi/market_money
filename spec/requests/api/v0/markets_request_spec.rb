d0d2e82df516f145206ef8718e9e20f8d9aff25c
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-15 22:23:51 -0400

    test: add tests for Get One Market happy and sad paths

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -1,36 +1,101 @@
 require 'rails_helper'
 
 describe 'Market Endpoints' do
-  it 'sends a list of all markets' do
-    market_1 = create(:market)
-    market_2 = create(:market)
-    market_3 = create(:market)
+  describe 'Get All Markets' do
+    it 'sends a list of all markets' do
+      market_1 = create(:market)
+      market_2 = create(:market)
+      market_3 = create(:market)
 
-    vendor_1 = create(:vendor)
-    vendor_2 = create(:vendor)
-    vendor_3 = create(:vendor)
+      vendor_1 = create(:vendor)
+      vendor_2 = create(:vendor)
+      vendor_3 = create(:vendor)
 
-    MarketVendor.create!(market: market_1, vendor: vendor_1)
-    MarketVendor.create!(market: market_1, vendor: vendor_2)
-    MarketVendor.create!(market: market_2, vendor: vendor_3)
+      MarketVendor.create!(market: market_1, vendor: vendor_1)
+      MarketVendor.create!(market: market_1, vendor: vendor_2)
+      MarketVendor.create!(market: market_2, vendor: vendor_3)
 
-    get '/api/v0/markets'
+      get '/api/v0/markets'
 
-    expect(response).to be_successful
-    expect(response.status).to eq(200)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
 
-    data = JSON.parse(response.body, symbolize_names: true)
-    expect(data).to have_key(:data)
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data).to have_key(:data)
 
-    markets = data[:data]
-    expect(markets).to be_an(Array)
-    expect(markets.count).to eq(3)
+      markets = data[:data]
+      expect(markets).to be_an(Array)
+      expect(markets.count).to eq(3)
 
-    markets.each do |market|
+      markets.each do |market|
+        expect(market).to be_a(Hash)
+
+        expect(market).to have_key(:id)
+        expect(market[:id]).to be_a(String)
+
+        expect(market).to have_key(:type)
+        expect(market[:type]).to be_a(String)
+        expect(market[:type]).to eq('market')
+
+        expect(market).to have_key(:attributes)
+        expect(market[:attributes]).to be_a(Hash)
+
+        attributes = market[:attributes]
+
+        expect(attributes).to have_key(:name)
+        expect(attributes[:name]).to be_a(String)
+
+        expect(attributes).to have_key(:street)
+        expect(attributes[:street]).to be_a(String)
+
+        expect(attributes).to have_key(:city)
+        expect(attributes[:city]).to be_a(String)
+
+        expect(attributes).to have_key(:county)
+        expect(attributes[:county]).to be_a(String)
+
+        expect(attributes).to have_key(:state)
+        expect(attributes[:state]).to be_a(String)
+
+        expect(attributes).to have_key(:zip)
+        expect(attributes[:zip]).to be_a(String)
+
+        expect(attributes).to have_key(:lat)
+        expect(attributes[:lat]).to be_a(String)
+
+        expect(attributes).to have_key(:lon)
+        expect(attributes[:lon]).to be_a(String)
+
+        expect(attributes).to have_key(:vendor_count)
+        expect(attributes[:vendor_count]).to be_an(Integer)
+      end
+    end
+  end
+
+  describe 'Get One Market' do
+    it 'can get one market by its id' do
+      market_1 = create(:market)
+
+      vendor_1 = create(:vendor)
+      vendor_2 = create(:vendor)
+
+      MarketVendor.create!(market: market_1, vendor: vendor_1)
+      MarketVendor.create!(market: market_1, vendor: vendor_2)
+
+      get "/api/v0/markets/#{market_1.id}"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data).to have_key(:data)
+
+      market = data[:data]
       expect(market).to be_a(Hash)
 
       expect(market).to have_key(:id)
-      expect(market[:id]).to be_an(Integer)
+      expect(market[:id]).to be_a(String)
+      expect(market[:id]).to eq(market_1.id.to_s)
 
       expect(market).to have_key(:type)
       expect(market[:type]).to be_a(String)
@@ -43,30 +108,54 @@ describe 'Market Endpoints' do
 
       expect(attributes).to have_key(:name)
       expect(attributes[:name]).to be_a(String)
+      expect(attributes[:name]).to eq(market_1.name)
 
       expect(attributes).to have_key(:street)
       expect(attributes[:street]).to be_a(String)
+      expect(attributes[:street]).to eq(market_1.street)
 
       expect(attributes).to have_key(:city)
       expect(attributes[:city]).to be_a(String)
+      expect(attributes[:city]).to eq(market_1.city)
 
       expect(attributes).to have_key(:county)
       expect(attributes[:county]).to be_a(String)
+      expect(attributes[:county]).to eq(market_1.county)
 
       expect(attributes).to have_key(:state)
       expect(attributes[:state]).to be_a(String)
+      expect(attributes[:state]).to eq(market_1.state)
 
       expect(attributes).to have_key(:zip)
       expect(attributes[:zip]).to be_a(String)
+      expect(attributes[:zip]).to eq(market_1.zip)
 
       expect(attributes).to have_key(:lat)
       expect(attributes[:lat]).to be_a(String)
+      expect(attributes[:lat]).to eq(market_1.lat)
+
 
       expect(attributes).to have_key(:lon)
       expect(attributes[:lon]).to be_a(String)
+      expect(attributes[:lon]).to eq(market_1.lon)
 
       expect(attributes).to have_key(:vendor_count)
       expect(attributes[:vendor_count]).to be_an(Integer)
+      expect(attributes[:vendor_count]).to eq(market_1.vendors.count)
+    end
+
+    it 'returns an error if an invalid market id is passed in' do
+
+      get "/api/v0/markets/5"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data).to have_key(:errors)
+
+      error = data[:errors][0][:detail]
+      expect(error).to eq("Couldn't find Market with 'id'=5")
     end
   end
 end
