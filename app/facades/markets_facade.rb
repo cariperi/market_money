03dbba6faf7401c6d7e0a9fec034611d5f21a098
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-15 22:25:49 -0400

    feat: add find_market(id) method to facade

--- app/facades/markets_facade.rb
+++ app/facades/markets_facade.rb
@@ -1,3 +1,9 @@
 class MarketsFacade
-
+  def self.find_market(id)
+    if Market.exists?(id: id)
+      Market.find(id)
+    else
+      ErrorMarket.new("Couldn't find Market with 'id'=#{id}", "NOT FOUND", 404)
+    end
+  end
 end
