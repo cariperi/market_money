7269e1f50d4e0bc7f0f42751b2689fffbdd9ed8f
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-16 13:07:37 -0400

    refactor: update controller index and show actions to use new serializer with gem

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,12 +1,12 @@
 class Api::V0::MarketsController < ApplicationController
   def index
-    render json: MarketSerializer.format_markets(Market.all)
+    render json: MarketSerializer.new(Market.all)
   end
 
   def show
     market = MarketsFacade.find_market(params[:id])
     if market.class == Market
-      render json: MarketSerializer.format_market(market)
+      render json: MarketSerializer.new(market)
     else
       render json: ErrorSerializer.format_error(market), status: 404
     end
