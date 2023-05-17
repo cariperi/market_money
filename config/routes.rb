3f40dd65b05237259f536bb88f9a1703f8ea407a
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-17 16:55:57 -0400

    feat: add route for markets search

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
+      get 'markets/search', to: 'markets#search'
+
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index], to: 'market_vendors#index'
       end
       resources :vendors, only: [:show, :create, :update, :destroy]
       resources :market_vendors, only: [:create]
-      delete 'market_vendors', action: :destroy, controller: 'market_vendors'
+      delete 'market_vendors', to: 'market_vendors#destroy'
     end
   end
 end
