d3bb5d4343211221bae0b73f28ec52e98f7fc994
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-17 12:39:21 -0400

    feat: edit syntax for validations in vendor model

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,9 +2,10 @@ class Vendor < ApplicationRecord
   has_many :market_vendors
   has_many :markets, through: :market_vendors
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :contact_name, presence: true
-  validates :contact_phone, presence: true
+  validates_presence_of :name,
+                        :description,
+                        :contact_name,
+                        :contact_phone
+
   validates :credit_accepted, exclusion: [nil]
 end
