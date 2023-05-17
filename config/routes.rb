ab0f372ccbf0f7cdd8320b60befdbd783968ceec
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-17 01:49:56 -0400

    feat: add route for create action for market vendor controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         resources :vendors, only: [:index], to: 'market_vendors#index'
       end
       resources :vendors, only: [:show, :create, :update, :destroy]
+      resources :market_vendors, only: [:create]
     end
   end
 end
